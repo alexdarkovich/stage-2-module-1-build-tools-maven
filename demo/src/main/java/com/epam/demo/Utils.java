0b19a1d601e6b202e322084e24869a7de6de3f84
AlexDark <alexdarkovich@mail.ru>
2023-03-21 14:44:53 +0300

    tasks implemented 2 attempt

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -6,10 +6,16 @@ import com.epam.utils.StringUtils;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
-       boolean flag = true;
-       for (String str : args) {
-           if (!StringUtils.isPositiveNumber(str)) flag = false;
-       }
-       return flag;
+        if(args == null || args.size() == 0)
+        {
+            return false;
+        } else {
+            for(String arg : args){
+                if (!StringUtils.isPositiveNumber(arg)){
+                    return false;
+                }
+            }
+            return true;
+        }
     }
 }
--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -2,11 +2,12 @@ package com.epam.utils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-        double number = Double.parseDouble(str);
-        if (number > 0) {
-            return true;
-        } else {
+        if(str == null ||str.equals("null") || str.equals("") || str.equals(" ") || str.startsWith("0"))
+        {
             return false;
+        } else {
+            double number = Double.parseDouble(str);
+            return number > 0;
         }
     }
 }
